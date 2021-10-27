08d2d8a40e491b80598e547564a5268d70c96fe7
MaximKotovich <makckotok2020@gmail.com>
2021-10-27 16:55:55 +0300

    update

--- todo/src/common/api/default-axios.api.js
+++
@@ -1,7 +0,0 @@
-import defaultAxios from 'axios'
-
-export const axios = defaultAxios.create({
-  baseURL: 'https://api-nodejs-todolist.herokuapp.com/user/register',
-  headers: { 'Content-Type': 'application/json' },
-  withCredentials: false,
-});
--- todo/src/components/todoList/api-for-todos/session-default-api.js
+++
@@ -1,4 +0,0 @@
-export const token = sessionStorage.getItem("token");
-export const config = {
-  headers: { Authorization: `Bearer ${token}` },
-};
